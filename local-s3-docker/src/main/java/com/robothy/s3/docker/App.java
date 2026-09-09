994e2aa5809e07e88971b6b50ca188b8fa023c67
linux_china <libing.chen@gmail.com>
2026-09-09 18:02:54 +0800

    chore: add AWS_BUCKETS support

--- local-s3-docker/src/main/java/com/robothy/s3/docker/App.java
+++ local-s3-docker/src/main/java/com/robothy/s3/docker/App.java
@@ -2,38 +2,45 @@ package com.robothy.s3.docker;
 
 import com.robothy.s3.rest.LocalS3;
 import com.robothy.s3.rest.bootstrap.LocalS3Mode;
+
 import java.util.Arrays;
 import java.util.Optional;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App {
 
-  private static final String MODE = "MODE";
+    private static final String MODE = "MODE";
 
-  public static void main(String[] args) {
-    if (getProperty(MODE) == null) {
-      log.info("\"MODE\" is not specified; use the default value \"PERSISTENCE\"");
+    public static void main(String[] args) {
+        if (getProperty(MODE) == null) {
+            log.info("\"MODE\" is not specified; use the default value \"PERSISTENCE\"");
+        }
+
+        final String mode = Optional.ofNullable(getProperty(MODE)).orElse(LocalS3Mode.PERSISTENCE.name());
+        if (Arrays.stream(LocalS3Mode.values()).noneMatch(m -> m.name().equalsIgnoreCase(mode))) {
+            log.error("\"{}\" is not a valid mode. Valid values are {}", mode, LocalS3Mode.values());
+            System.exit(1);
+        }
+
+        log.info("Starting LocalS3 in {} mode.", mode);
+
+        String[] buckets = null;
+        if (System.getenv("AWS_BUCKETS") != null) {
+            buckets = System.getenv("AWS_BUCKETS").split(",");
+        }
+        LocalS3.builder()
+                .port(80)
+                .mode(LocalS3Mode.valueOf(mode.toUpperCase()))
+                .dataPath("/data")
+                .buckets(buckets)
+                .build()
+                .start();
     }
 
-    final String mode = Optional.ofNullable(getProperty(MODE)).orElse(LocalS3Mode.PERSISTENCE.name());
-    if (Arrays.stream(LocalS3Mode.values()).noneMatch(m -> m.name().equalsIgnoreCase(mode))) {
-      log.error("\"{}\" is not a valid mode. Valid values are {}", mode, LocalS3Mode.values());
-      System.exit(1);
+    private static String getProperty(String name) {
+        return Optional.ofNullable(System.getenv(name)).orElse(System.getProperty(name));
     }
-
-    log.info("Starting LocalS3 in {} mode.", mode);
-
-    LocalS3.builder()
-        .port(80)
-        .mode(LocalS3Mode.valueOf(mode.toUpperCase()))
-        .dataPath("/data")
-        .build()
-        .start();
-  }
-
-  private static String getProperty(String name) {
-    return Optional.ofNullable(System.getenv(name)).orElse(System.getProperty(name));
-  }
 
 }
